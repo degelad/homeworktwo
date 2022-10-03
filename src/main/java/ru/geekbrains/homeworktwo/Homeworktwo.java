21ece25ce189fafc686187b911f0fd34a688cd1e
degelad <degelad@yandex.ru>
2022-10-03 16:05:21 +0500

    empty commit

---
+++ src/main/java/ru/geekbrains/homeworktwo/Homeworktwo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ru.geekbrains.homeworktwo;
+
+/**
+ *
+ * @author degelad
+ */
+public class Homeworktwo {
+
+    public static void main(String[] args) {
+
+    }
+}
